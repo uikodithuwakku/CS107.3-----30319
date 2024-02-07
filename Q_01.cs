75e59601e11d90ccfbd320ac0dd1c6f118d77e00
uikodithuwakku <135606484+uikodithuwakku@users.noreply.github.com>
2024-02-07 16:03:06 +0530

    Lab session 02

---
+++ Q_01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02
+{
+    /*public class Book
+    {
+        public string title = "Famous Five";
+        string author = "Enid Blyton";
+
+        static void Main()
+        {
+            Book btitle = new Book();
+            Book bauthor = new Book();
+            Console.WriteLine(btitle.title);
+            Console.WriteLine(bauthor.author);
+            Console.ReadLine();
+        }   
+    }*/
+}
---
+++ Q_02.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02
+{
+    /*public class BankAccount
+    {
+        public int AccountNumber;
+        public double balance;
+
+        public BankAccount(double AccBalance)
+        {
+            balance = AccBalance;
+        }
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            BankAccount myAccount = new BankAccount(500);
+
+            Console.Write("Account Number :");
+            myAccount.AccountNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Deposit Amount :");
+            double amount = Convert.ToInt32(Console.ReadLine());
+
+            myAccount.Deposit(amount);
+
+            Console.WriteLine("Account Number : " + myAccount.AccountNumber);
+            Console.WriteLine("Updated Balance : " + myAccount.balance);
+            Console.Read();
+        }
+    } */  
+}
