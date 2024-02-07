695f418f3e73f8ba8d4b68cf314baaf1e31ad34d
uikodithuwakku <135606484+uikodithuwakku@users.noreply.github.com>
2024-02-07 16:56:32 +0530

    Lab session 02

---
+++ Lab_02/Q_03.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02
+{
+    public class TemperatureTracker
+    {
+        public double[] Temperature = new double[7];
+
+        public void dailyTemp()
+        {
+            for(int i = 0;i<7;i++)
+            {
+                Console.Write($"Enter {i+1} day temperature : ");
+                Temperature[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        public void Report()
+        {
+            Console.WriteLine("Weekly Teperature Report : "); ;
+
+            for(int i = 0;i<7; i++)
+            {
+                Console.WriteLine($"Day {i + 1} : Teperature : {Temperature[i]} celcious");
+            }
+        }
+        
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            TemperatureTracker tracker = new TemperatureTracker();
+            tracker.dailyTemp();
+            Console.WriteLine("\n");
+            tracker.Report();
+            Console.Read();
+
+        }
+    }
+}
---
+++ Lab_02/Q_04.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02
+{
+    public class Product
+    {
+        public string ProductName;
+        public double price;
+
+        public Product(string productName,double ProductPrice)
+        {
+            ProductName = productName;
+            price = ProductPrice;
+        }
+
+        public void ProductDetails()
+        {
+            Console.WriteLine("Product Details : ");
+            Console.WriteLine($"Product Name : {ProductName}");
+            Console.WriteLine($"Price : {price}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Product p1 = new Product("smart phone" , 120000.00);
+            p1.ProductDetails();
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
