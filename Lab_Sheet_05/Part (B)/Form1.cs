f8e574cc40d61cfeed84db332b0a81283a12fc07
uikodithuwakku <135606484+uikodithuwakku@users.noreply.github.com>
2024-03-18 09:47:34 +0530

    Did Lab sheet 05 - B

---
+++ Lab_Sheet_05/Part (B)/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace Lab_05_B_
+{
+    public partial class Form1 : Form
+    {
+        private void butReset_Click(object sender, EventArgs e)
+        {
+            txtFname.Text = "";
+            txtLname.Text = "";
+            dateTimePicker1.Text = "";
+            txtEmail.Text = "";
+            txtUname.Text = "";
+            txtPwd.Text = "";
+            txtConfirmPwd.Text = "";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butSignup_Click(object sender, EventArgs e)
+        {
+             if(string.IsNullOrEmpty(txtFname.Text) || string.IsNullOrEmpty(txtLname.Text) || string.IsNullOrEmpty(dateTimePicker1.Text) || string.IsNullOrEmpty(txtEmail.Text) || string.IsNullOrEmpty(txtUname.Text) || string.IsNullOrEmpty(txtPwd.Text) || string.IsNullOrEmpty(txtConfirmPwd.Text))
+             {
+                 MessageBox.Show("Input fields are empty....", "Error", MessageBoxButtons.OK , MessageBoxIcon.Error);
+             }
+
+             else if(txtPwd.Text != txtConfirmPwd.Text)
+             {
+                 MessageBox.Show("Psswords are doesn't match....", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+             }
+
+             else
+             {
+                Form2 form2 = new Form2();
+                form2.passingvalue = txtFname.Text + " " + txtLname.Text;
+                form2.passingvalue1 = txtUname.Text; 
+                form2.passingvalue2 = txtEmail.Text;
+                form2.Show();
+                this.Hide();
+             }
+
+            
+        }
+    }
+}
---
+++ Lab_Sheet_05/Part (B)/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_05_B_
+{
+    public partial class Form2 : Form
+    {
+        public string name, Uname, Email;
+
+        public string passingvalue
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string passingvalue1
+        {
+            get { return Uname; }
+            set { Uname = value; }
+        }
+
+        public string passingvalue2
+        {
+            get { return Email; }
+            set { Email = value; }
+        }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void lblName_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            lblName.Text = "" + name;
+            lblUname.Text = "" +Uname;
+            lblEmail.Text = "" + Email;
+        }
+    }
+}
