41fadcc97c6c21f038c387756a0693e3519845e8
uikodithuwakku <135606484+uikodithuwakku@users.noreply.github.com>
2024-04-03 20:13:00 +0530

    Lab Sheet 07

---
+++ Lab_07/BankAccount.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+
+    public abstract class BankAccount
+    {
+        public abstract void deposit(double amount);
+        public abstract void withdraw(double amount);
+
+        public double balance;
+
+        public BankAccount(double initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public void displayBalance()
+        {
+            Console.WriteLine($"Current balance : {balance}");
+        }
+
+    }
+
+    public class SavingAccount : BankAccount
+    {
+        public SavingAccount(double initialBalance) : base(initialBalance) { }
+        public override void deposit(double amount)
+        {
+            balance += amount;
+        }
+        public override void withdraw(double amount)
+        {
+            if(balance - amount >= 0)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance.....");
+            }
+        }
+    }
+
+    public class CurrentAccount : BankAccount
+    {
+        public CurrentAccount(double initialBalance) : base(initialBalance) { }
+        public override void deposit(double amount)
+        {
+            balance += amount;
+        }
+        public override void withdraw(double amount)
+        {
+            balance -= amount;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SavingAccount sa = new SavingAccount(1000);
+            CurrentAccount ca = new CurrentAccount(2000);
+
+            Console.WriteLine("Choose the Account type :");
+            Console.WriteLine("1. Saving Account \t\t 2.Current Account");
+            int AccType = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose the Transaction type :");
+            Console.WriteLine("1. Deposit \t\t 2.Withdraw");
+            int TransactionType = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Amount :");
+            double amount = double.Parse(Console.ReadLine());   
+
+            if(AccType == 1)
+            {
+                if(TransactionType == 1)
+                {
+                    sa.deposit(amount);
+                }
+                else
+                {
+                    sa.withdraw(amount);
+                }
+                sa.displayBalance();
+            }
+            else if (AccType == 2) 
+            {
+                if (TransactionType == 1)
+                {
+                    ca.deposit(amount);
+                }
+                else
+                {
+                    ca.withdraw(amount);
+                }
+                ca.displayBalance();
+            }
+            else
+            {
+                Console.WriteLine("Choose account type or transaction type");
+            }
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab_07/ZooManagement.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    public abstract class Animal
+    {
+        public abstract void MakeSound();
+      
+    }
+
+    public class Lion : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Lion Roars");
+        }
+    }
+
+    public class Tiger : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Tiger Growls");
+        }
+    }
+
+    public class Elephant : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Elephant Trumpets");
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            Console.WriteLine("Enter the type of the animal :");
+            string animalType = Console.ReadLine();
+            
+            Animal animalSound = null;
+            switch(animalType.ToLower())
+            {
+                case "tiger": animalSound = new Tiger(); break;
+                case "lion": animalSound = new Lion(); break;
+                case "elephant": animalSound = new Elephant(); break;
+            }
+            
+            animals.Add(animalSound);
+            Console.WriteLine("Animal is added");
+            Console.WriteLine("The sound of the added animal :");
+
+            foreach(Animal animal in animals)
+            {
+                animal.MakeSound();
+            }
+            Console.ReadLine();
+        }
+    }
+}
