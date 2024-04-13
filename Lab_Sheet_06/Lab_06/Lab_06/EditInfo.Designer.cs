4061e78e2212e90dbc74f9af80aa20174bafc0e4
uikodithuwakku <135606484+uikodithuwakku@users.noreply.github.com>
2024-04-13 10:44:21 +0530

    Reupload the Lab Sheet 06

---
+++ Lab_Sheet_06/Lab_06/Lab_06/EditInfo.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Lab_06
+{
+    partial class EditInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.butSearch = new System.Windows.Forms.Button();
+            this.butUpdate = new System.Windows.Forms.Button();
+            this.butShow = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUname
+            // 
+            this.txtUname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUname.Location = new System.Drawing.Point(270, 47);
+            this.txtUname.Name = "txtUname";
+            this.txtUname.Size = new System.Drawing.Size(265, 35);
+            this.txtUname.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 29);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "UserName";
+            // 
+            // butSearch
+            // 
+            this.butSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butSearch.Location = new System.Drawing.Point(640, 31);
+            this.butSearch.Name = "butSearch";
+            this.butSearch.Size = new System.Drawing.Size(155, 51);
+            this.butSearch.TabIndex = 20;
+            this.butSearch.Text = "Search";
+            this.butSearch.UseVisualStyleBackColor = true;
+            this.butSearch.Click += new System.EventHandler(this.butSearch_Click);
+            // 
+            // butUpdate
+            // 
+            this.butUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butUpdate.Location = new System.Drawing.Point(391, 632);
+            this.butUpdate.Name = "butUpdate";
+            this.butUpdate.Size = new System.Drawing.Size(155, 51);
+            this.butUpdate.TabIndex = 25;
+            this.butUpdate.Text = "Update";
+            this.butUpdate.UseVisualStyleBackColor = true;
+            this.butUpdate.Click += new System.EventHandler(this.butUpdate_Click);
+            // 
+            // butShow
+            // 
+            this.butShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butShow.Location = new System.Drawing.Point(640, 632);
+            this.butShow.Name = "butShow";
+            this.butShow.Size = new System.Drawing.Size(155, 51);
+            this.butShow.TabIndex = 26;
+            this.butShow.Text = "Display";
+            this.butShow.UseVisualStyleBackColor = true;
+            this.butShow.Click += new System.EventHandler(this.butShow_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(417, 435);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(265, 35);
+            this.txtEmail.TabIndex = 36;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(205, 441);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 29);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Email";
+            // 
+            // txtNo
+            // 
+            this.txtNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNo.Location = new System.Drawing.Point(417, 364);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(265, 35);
+            this.txtNo.TabIndex = 34;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(205, 370);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 29);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "Contact No";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(417, 300);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(265, 35);
+            this.txtAddress.TabIndex = 32;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(205, 306);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 29);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Address";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(417, 231);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 35);
+            this.dateTimePicker1.TabIndex = 30;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(205, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 29);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "DOB";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(417, 155);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(265, 35);
+            this.txtName.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(205, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 29);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Name";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPwd.Location = new System.Drawing.Point(417, 521);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(265, 35);
+            this.txtPwd.TabIndex = 38;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(205, 527);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 29);
+            this.label7.TabIndex = 37;
+            this.label7.Text = "Password";
+            // 
+            // EditInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 711);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.butShow);
+            this.Controls.Add(this.butUpdate);
+            this.Controls.Add(this.butSearch);
+            this.Controls.Add(this.txtUname);
+            this.Controls.Add(this.label3);
+            this.Name = "EditInfo";
+            this.Text = "Update";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button butSearch;
+        private System.Windows.Forms.Button butUpdate;
+        private System.Windows.Forms.Button butShow;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ Lab_Sheet_06/Lab_06/Lab_06/Employee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab_06
+{ 
+    public partial class Employee : System.Windows.Forms.Form
+    {
+        SqlConnection con1 = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Laptop Outlet\OneDrive\Y1 - S3\C# Lab\Lab_06\Lab_06\Database1.mdf"";Integrated Security=True");
+        public Employee()
+        {
+            InitializeComponent();
+        }
+
+        private void butEnter_Click(object sender, EventArgs e)
+        {
+            String Name = txtName.Text;
+            String Dob = dateTimePicker1.Text;
+            String Address = txtAddress.Text;
+            int ConNo = int.Parse(txtNo.Text);
+            String Email = txtEmail.Text;
+            String Uname = txtUname.Text;
+            String pwd = txtPwd.Text;
+
+            String query = $"INSERT INTO Employee (Name,DOB,UserName,Password) VALUES ('{Name}','{Dob}','{Uname}','{pwd}')" ;
+            SqlCommand cmd = new SqlCommand(query, con1);
+
+            String query2 = $"INSERT INTO Contact (UserName,ContactNo,Email,Address) VALUES ('{Uname}','{ConNo}','{Email}','{Address}')";
+            SqlCommand cmd2 = new SqlCommand(query2, con1);
+
+            try
+            {
+                con1.Open();
+                cmd.ExecuteNonQuery();
+                con1.Close();
+
+                con1.Open();
+                cmd2.ExecuteNonQuery();
+                MessageBox.Show("Registration Sucessfully....");
+                con1.Close();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error..." + ex.Message);
+            }
+            Reset();
+
+        }
+
+        private void butReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            txtName.Text = "";
+            dateTimePicker1.Text = "";
+            txtAddress.Text = "";
+            txtNo.Text = "";
+            txtEmail.Text = "";
+            txtUname.Text = "";
+            txtPwd.Text = "";
+        }
+    }
+}
